430654085f24d94064f0c08d04ceed9f6e9494f8
vinayaksutar-work <vinayaksutar.work@gmail.com>
2024-02-06 19:08:42 +0530

    set students url as home page

--- app/Views/layouts/navbar.php
+++ app/Views/layouts/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-    <a class="navbar-brand" href="<?= base_url('students') ?>">Students</a>
+    <a class="navbar-brand" href="<?= base_url('/') ?>">Students</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
