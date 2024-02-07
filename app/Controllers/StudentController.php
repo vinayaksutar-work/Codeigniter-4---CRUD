c243d113ca962f568edfafd040445adebf028724
vinayaksutar-work <vinayaksutar.work@gmail.com>
2024-02-07 18:56:23 +0530

    add flashdata status in delete function

--- app/Controllers/StudentController.php
+++ app/Controllers/StudentController.php
@@ -55,9 +55,15 @@ class StudentController extends BaseController
     {
         $student = new StudentModel();
         $student->delete($id);
-        session()->setFlashdata('status_text', 'Your student Data has been deleted');
-        return redirect()->back()
-            ->with('status_icon', 'error')
-            ->with('status', 'Student Deleted Successfully!!!');
+        // session()->setFlashdata('status_text', 'Your student Data has been deleted');
+        // return redirect()->back()
+        //     ->with('status_icon', 'error')
+        //     ->with('status', 'Student Deleted Successfully!!!');
+        $data = [
+            'status' => "Deleted Successfully!!!",
+            'status_text' => "Your status data han been deleted successfully",
+            'status_icon' => 'success',
+        ];
+        return $this->response->setJSON($data);
     }
 }
