b57e2e29b98044188b7469ae01cc21ea148024cc
vinayaksutar-work <vinayaksutar.work@gmail.com>
2024-02-08 19:08:49 +0530

    app/Config/Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,7 +10,7 @@ $routes->get('students/create', 'StudentController::create');
 $routes->post('students/store', 'StudentController::store');
 $routes->get('student/edit/(:num)', 'StudentController::edit/$1');
 $routes->put('student/update/(:num)', 'StudentController::update/$1');
-$routes->get('student/confirm-delete/(:num)', 'StudentController::delete/$1');
+$routes->get('student/delete/(:num)', 'StudentController::delete/$1');
 
 // $routes->get('products', 'ProductController::index');
 // $routes->get('products/(:any)', 'ProductController::find/$1');
