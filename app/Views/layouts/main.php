4ae62f110a0922ea1683fe6271877f3bcbe9feff
vinayaksutar-work <vinayaksutar.work@gmail.com>
2024-02-08 18:59:50 +0530

    add style.css url

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
 <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="<?= base_url("assets/css/style.css") ?>">
 <link rel="stylesheet" href="//cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
 <title>Codeigniter 4</title>
 </head>
